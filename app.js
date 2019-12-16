b8ec27a8adb4ef3583d81a7e5a4e9f863c150175
Mark-Ball <markball111@gmail.com>
2019-12-17 07:46:14 +1100

    completed assignment of left and right sides in question constructor

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 const falsyValues = [0, null, undefined, '', false, NaN];
 const truthyValues = [1, 5, 'hello', 7, true];
-const trickValues = ['null', 'false', [], {}, 'zero'];
+const trickValues = ['null', 'false', [], {}, 'zero', '0'];
 
 // 50% &&, 50% ||
-// 30% falsy vs falsy
-// 15% falsy vs truthy
-// 15% truthy vs falsy
-// 30% truthy vs truthy
-// 5% trick vs falsy
-// 5% trick vs truthy
+// 20% falsy vs falsy
+// 20% falsy vs truthy
+// 20% truthy vs falsy
+// 20% truthy vs truthy
+// 4% trick vs falsy
+// 4% trick vs truthy
+// 4% falsy vs trick
+// 4% truthy vs trick
+// 4% trick vs trick
 
 class Question {
     constructor() {
         let randomType = Math.random();
 
-        if (randomType <= 0.45) {
+        if (randomType <= 0.20) {
             this.left = falsyValues[Math.floor(Math.random() * falsyValues.length + 1)];
-        } else if (randomType <= 0.9) {
+            this.right = falsyValues[Math.floor(Math.random() * falsyValues.length + 1)];
+        } else if (randomType <= 0.40) {
+            this.left = falsyValues[Math.floor(Math.random() * falsyValues.length + 1)];
+            this.right = truthyValues[Math.floor(Math.random() * truthyValues.length + 1)];
+        } else if (randomType <= 0.60) {
             this.left = truthyValues[Math.floor(Math.random() * truthyValues.length + 1)];
+            this.right = falsyValues[Math.floor(Math.random() * falsyValues.length + 1)];
+        } else if (randomType <= 0.80) {
+            this.left = truthyValues[Math.floor(Math.random() * truthyValues.length + 1)];
+            this.right = truthyValues[Math.floor(Math.random() * truthyValues.length + 1)];
+        } else if (randomType <= 0.84) {
+            this.left = trickValues[Math.floor(Math.random() * trickValues.length + 1)];
+            this.right = falsyValues[Math.floor(Math.random() * falsyValues.length + 1)];
+        } else if (randomType <= 0.88) {
+            this.left = trickValues[Math.floor(Math.random() * trickValues.length + 1)];
+            this.right = truthyValues[Math.floor(Math.random() * truthyValues.length + 1)];
+        } else if (randomType <= 0.92) {
+            this.left = falsyValues[Math.floor(Math.random() * falsyValues.length + 1)];
+            this.right = trickValues[Math.floor(Math.random() * trickValues.length + 1)];
+        } else if (randomType <= 0.96) {
+            this.left = truthyValues[Math.floor(Math.random() * truthyValues.length + 1)];
+            this.right = trickValues[Math.floor(Math.random() * trickValues.length + 1)];
         } else {
             this.left = trickValues[Math.floor(Math.random() * trickValues.length + 1)];
+            this.right = trickValues[Math.floor(Math.random() * trickValues.length + 1)];
         }
     }
 
@@ -28,7 +52,7 @@ class Question {
     }
 
     values() {
-        return [this.left];
+        return [this.left, this.right];
     }
 }
 
