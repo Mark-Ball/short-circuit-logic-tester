b94bd7068dbee23f00d27e4b7ea1c7ee2570dab4
Mark-Ball <markball111@gmail.com>
2020-01-04 16:14:49 +1100

    converted controller to use quiz entered into database rather than as variable

--- controllers/pages_controller.js
+++ controllers/pages_controller.js
@@ -1,9 +1,6 @@
 const Quiz = require('./../classes/Quiz');
 const ScorerModel = require('./../database/models/scorer_model');
 const QuizModel = require('./../database/models/quiz_model');
-let quiz1;
-let quizScore;
-let quizTime;
 
 function landing(req, res) {
     res.render('landing');
@@ -16,46 +13,60 @@ async function quiz(req, res) {
         quiz: quiz1
     });
 
-    quizTime = 0;
     res.render('quiz', { questions, quizId });
 }
 
-function checkResponses(req, res) {
-    console.log(req.body.quizId);
-    
-    quizScore = 0;
+async function checkResponses(req, res) {
+    // find the correct quiz from the database
+    const quiz1 = await QuizModel.findById(req.body.quizId);
+    const questions = quiz1.quiz.questions;
+
+    // find the time the player took and the responses they gave
     const { time, responses } = JSON.parse(req.body.data);
-    quizTime = time;
+
+    //initialise the quizResponse object, which will be sent to the report page
     let quizResponse = {
         score: 0,
         time: time,
         questions: []
     };
 
-    for (i in quiz1.questions) {
+    // iterate through all questions, add text, answers, and response to the questionData object
+    for (i in questions) {
         let questionData = {};
-        questionData.text = quiz1.questions[i].text;
-        questionData.answer = `${quiz1.questions[i].correctAnswer}`;
+        questionData.text = questions[i].text;
+        questionData.answer = `${questions[i].correctAnswer}`;
         if (questionData.answer === '') {
             questionData.answer = '""';
         }
         questionData.response = responses[i];
+
+        // push questionData object onto array inside quizResponse
         quizResponse.questions.push(questionData);
 
-        if (`"${quiz1.questions[i].correctAnswer}"` === `"${responses[i]}"`) {
+        // score the responses provided
+        if (`"${questions[i].correctAnswer}"` === `"${responses[i]}"`) {
             quizResponse.score += 1;
-            quizScore += 1;
         }
     }
-    res.render('report', { quizResponse });
+
+    // write score and time to the database
+    // name will be added in an update
+    const { _id: scorerId } = await ScorerModel.create({
+        name: 'Placeholder',
+        score: quizResponse.score,
+        time: quizResponse.time
+    });
+
+    res.render('report', { quizResponse, scorerId });
 }
 
+// since the score and time were recorded in the previous method
+// this method only updates the name
 async function recordScore(req, res) {
     const name = req.body.name === '' ? 'Unknown' : req.body.name;
-    await ScorerModel.create({
-        name: name,
-        score: quizScore,
-        time: quizTime
+    await ScorerModel.findByIdAndUpdate(req.body.scorerId, {
+        name: name
     });
     res.redirect('/leaderboard');
 }
