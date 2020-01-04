a988fd25c9f2ada3b8ae1bc2ee478319c900b128
Mark-Ball <markball111@gmail.com>
2020-01-05 09:09:21 +1100

    configured mongodb to ignore undefined values, which was causing a bug trying to write undefined into the database

--- database/connect.js
+++ database/connect.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_HOST, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    ignoreUndefined: true
 });
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', error => console.log(error));
